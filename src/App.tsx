0993e74070081a557da200b20df44f0b1e8199bc
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-06-21 14:32:25 -0300

    adding functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 
-const COLORS: [
+type Color = {
+  name: string;
+  color: string;
+};
+
+const COLORS: Color[] = [
   {
-    name: "rojo";
-    color: "#f00";
+    name: "rojo",
+    color: "#f00",
   },
   {
-    name: "verde";
-    color: "#0f0";
+    name: "verde",
+    color: "#0f0",
   },
   {
-    name: "azul";
-    color: "#00f";
+    name: "azul",
+    color: "#00f",
   },
   {
-    name: "amarillo";
-    color: "#ff0";
+    name: "amarillo",
+    color: "#ff0",
   },
 ];
 
@@ -23,6 +28,19 @@ function App() {
   const [status, setStatus] = useState<"initial" | "playing" | "finished">("initial");
   const [time, setTime] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
+  const [color, setColor] = useState<null | Color>(null);
+  const [wrongColor, setWrongColor] = useState<null | Color>(null);
+
+  function handlePlay() {
+    setStatus("playing");
+    setTime(0);
+    setScore(0);
+
+    const [color, wrongColor] = COLORS.slice().sort(() => Math.random() - 0.5);
+
+    setColor(color);
+    setWrongColor(wrongColor);
+  }
 
   useEffect(() => {
     let interval: number;
@@ -46,16 +64,17 @@ function App() {
       </header>
       {status === "playing" && (
         <section>
-          <span>Blanco</span>
+          <span style={{ textTransform: "capitalize", color: wrongColor?.color }}>{color?.name}</span>
         </section>
       )}
       <footer>
-        {status === "initial" && <button onClick={() => setStatus("playing")}>Jugar</button>}
+        {status === "initial" && <button onClick={handlePlay}>Jugar</button>}
         {status === "finished" && <button onClick={() => setStatus("initial")}>Reiniciar</button>}
-        {status === "playing" && 
-        <>
-        </>
-        }
+        {status === "playing" && color && wrongColor && (
+          <>
+            <button onClick={() => handleColorClick(color)} style={{ width: 128, height: 128, backgroundColor: color.color }} />
+            <button onClick={() => handleColorClick(wrongColor)} style={{ width: 128, height: 128, backgroundColor: wrongColor.color }} />
+          </>)}
       </footer>
     </main>
   );
